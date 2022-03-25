5d891970cf5f682994f98b3ac58d18c700c8c48a
gabrielhofmann <hofmannmarinho@gmail.com>
2022-03-25 12:54:37 -0300

    update

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,21 +1,34 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
-import logo from '../assets/logo.svg';
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.svg";
 import "./header.scss";
 
 export default function Header() {
   return (
     <Navbar className="header" fixed="top" expand="lg">
       <Container className="header-container">
-        <Navbar.Brand href="/"><img className="header-logo" src={logo} alt="RanGo" /></Navbar.Brand>
+        <Navbar.Brand href="/">
+          <img className="header-logo" src={logo} alt="RanGo" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link className="header-link" href="/">Home</Nav.Link>
-            <Nav.Link className="header-link" href="/saibamais">Sobre</Nav.Link>
-            <Nav.Link className="header-link" href="/noticias">Conteúdo</Nav.Link>
-            <Nav.Link className="header-link" href="#sejaparceiro">Seja parceiro</Nav.Link>
-            <Nav.Link className="header-link" href="/plataforma">Plataforma RanGo</Nav.Link>
+            <Link className="header-link" href="/">
+              Home
+            </Link>
+            <Link className="header-link" href="/saibamais">
+              Sobre
+            </Link>
+            <Link className="header-link" href="/noticias">
+              Conteúdo
+            </Link>
+            <Link className="header-link" href="#sejaparceiro">
+              Seja parceiro
+            </Link>
+            <Link className="header-link" href="/plataforma">
+              Plataforma RanGo
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
