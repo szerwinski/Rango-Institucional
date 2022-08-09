b7dded7eb57cc6448adf1736a1363829f58c7252
gabrielhofmann <hofmannmarinho@gmail.com>
2022-08-09 16:51:41 -0300

    links page  && download link background in header

--- src/pages/linksPage.jsx
+++ src/pages/linksPage.jsx
@@ -2,50 +2,47 @@ import React from "react";
 import Footer from "../components/footer";
 import Header from "../components/header";
 import "./linksPage.scss";
+import image from "../assets/linksImage.png";
 import insta from "../assets/insta.png";
-import fb from "../assets/fb.svg";
+import fb from "../assets/fb.png";
 import tiktok from "../assets/tiktok.png";
-import twitter from "../assets/twitter.svg";
-import icon2 from "../assets/icon2.svg";
+import twitter from "../assets/twitter.png";
 
 export default function LinksPage() {
   return (
     <main>
       <Header />
 
-      <section className="linksContent">
-        <h1>Confira nossas redes sociais:</h1>
-
+      <section className="links">
         <div className="linksContainer">
-          <a
-            href="https://www.instagram.com/rangosemfila/?hl=en"
-            className="mediaLink"
-          >
-            <p>Instagram</p>
+          <ul>
+            <li>
+              <img src={fb} alt="" />
 
-            <img src={insta} alt="" />
-          </a>
+              <i>/rangosemfila</i>
+            </li>
+            <li>
+              <img src={insta} alt="" />
 
-          <a
-            href="https://www.facebook.com/rangosemfila/"
-            className="mediaLink"
-          >
-            Facebook
-            <img src={fb} alt="" />
-          </a>
+              <i>@rangosemfila</i>
+            </li>
+            <li>
+              <img src={tiktok} alt="" />
 
-          <a href="https://twitter.com/rangosemfila" className="mediaLink">
-            Twitter
-            <img src={twitter} alt="" />
-          </a>
+              <i>@rangosemfila</i>
+            </li>
+            <li>
+              <img src={twitter} alt="" />
 
-          <a href="https://www.tiktok.com/@rangosemfila" className="mediaLink">
-            Tiktok
-            <img src={tiktok} alt="" />
-          </a>
+              <i>@rangosemfila</i>
+            </li>
+          </ul>
         </div>
 
-        <img src={icon2} alt="" id="linksImage" />
+        <div
+          className="linksImage"
+          style={{ backgroundImage: `url(${image})` }}
+        ></div>
       </section>
 
       <Footer />
