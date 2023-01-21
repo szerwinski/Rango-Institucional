71051b89fed738c702e207ae9b4b763f3a4eb0b9
lucasmma <lukito020@gmail.com>
2023-01-21 17:02:04 -0300

    feat: sendForm to backend and showSucessPage

--- src/pages/suporte.jsx
+++ src/pages/suporte.jsx
@@ -1,11 +1,13 @@
 import styled from "styled-components"
 import Flexbox from "../styles/flexbox"
 import AnimacaoSuporte from "../assets/gif/animação_suporte.gif"
+import AnimacaoConcluído from "../assets/gif/animacao-concluido.gif"
 import BaseInput from "../styles/input/base-input"
 import BaseTextAreaInput from "../styles/input/base-text-area-input"
 import SizedDivisor from "../styles/sized-divisor"
 import Header from "../components/header";
 import React from "react"
+import axios from "axios"
 
 const Background = styled(Flexbox)`
   width: 100vw;
@@ -91,87 +93,143 @@ const RadioButton = styled.input`
 export default function SuportScreen() {
   const [isCellphone] = React.useState(window.innerWidth <= 800)
   const [contactForm, setContactForm] = React.useState(null)
+  const [restaurantName, setRestaurantName] = React.useState(null)
+  const [name, setName] = React.useState(null)
+  const [contact, setContact] = React.useState(null)
+  const [problemDescription, setProblemDescription] = React.useState(null)
+  const [showSucessPage, setShowSucessPage] = React.useState(false)
+
+  async function sendSupportEmail(name, restaurantName, contact, problemDescription) {
+    const body = { restaurantName, name, problemDescription }
+    if(contactForm === "mail") {
+      body.email = contact
+    } else {
+      body.phoneNumber = contact
+    }
+    console.log(body)
+    const response = await  axios.post(
+      "http://localhost:1337/v2/sendSuportFormMail",
+      body
+    )
+    console.log(response)
+    return response.status === 200
+  }
+
+  function validateForm() {
+    const form = [name, restaurantName, contactForm, contact, problemDescription]
+    const formLabels = ["Nome", 'Restaurante', 'Tipo de contato', "Contato", "Seu problema"]
+    if(form.includes(null) || form.includes(undefined)) {
+      var warningMessage = "Preencha as informações abaixo:\n"
+      form.map((value, index) => {
+        if(value == null || value == undefined) { 
+          warningMessage += ("- " + formLabels[index] + "\n")
+        }
+      })
+      alert( warningMessage)
+      return false
+    }
+    return true
+  }
 
   return <Background>
     <Header/>
     <Flexbox flexDirection='column' style={{ height: '100%', width: '100%' }}>
-    <Flexbox style={{ backgroundColor: '#E8E8E8', padding: '40px 0px', width: '100%' }}>
       {
-      isCellphone  ? <div/> :
+        showSucessPage ? <Flexbox flexDirection='column'>
+          <Image src={AnimacaoConcluído}/>
+          <Description>Obrigado! Nossos especialistas entrarão em contato o quanto antes :)</Description>
+        </Flexbox> :   <Flexbox style={{ backgroundColor: '#E8E8E8', padding: '40px 0px', width: '100%' }}>
+        {
+        isCellphone  ? <div/> :
+          <Container>
+            <Image src={AnimacaoSuporte}/>
+          </Container>
+        }
+        {
+        isCellphone  ? <div/> :
+          <VerticalLine/>
+        }
         <Container>
-          <Image src={AnimacaoSuporte}/>
-        </Container>
-      }
-      {
-      isCellphone  ? <div/> :
-        <VerticalLine/>
-      }
-      <Container>
-        <FormWrapper flexDirection='column' style={{ height: '100%' }}>
-          {
-            isCellphone  ? <SizedDivisor style={{ height: '15vh' }}/>: <div/>
-          }
-          <Title>Rango Suporte</Title>
-          <HorizontalLine/>
-          <Description>Preencha o formulário abaixo para nos enviar uma mensagem de suporte Responderemos assim que possível.</Description>
-          {
-            isCellphone  ? 
-              <Flexbox flexDirection='column'>
-                <SizedDivisor height='40'/>
-                <Image src={AnimacaoSuporte}/>
-              </Flexbox> : <div/>
-          }
-          <SizedDivisor height='40'/>
-          <Flexbox style={{width: 'calc(100% - 20px)', margin: '0px 50px'}}>
-            <Input placeholder='Nome' type='string'/>
-            <SizedDivisor width='40'/>
-            <Input placeholder='Restaurante' type='string' />
-          </Flexbox>
-          <SizedDivisor height='20'/>
-          <Flexbox style={{width: 'calc(100% - 20px)' }}>
-            <Flexbox>
-              <RadioButton type="radio" name="contact" value="phone" onChange={ (value) => {
-                  if(value.target.checked) {
-                    setContactForm('phone')
-                  }
-                }
-              }/>
-              <SizedDivisor width='8'/>
-              <RadioLabel>Telefone</RadioLabel>
+          <FormWrapper flexDirection='column' style={{ height: '100%' }}>
+            {
+              isCellphone  ? <SizedDivisor style={{ height: '15vh' }}/>: <div/>
+            }
+            <Title>Rango Suporte</Title>
+            <HorizontalLine/>
+            <Description>Preencha o formulário abaixo para nos enviar uma mensagem de suporte Responderemos assim que possível.</Description>
+            {
+              isCellphone  ? 
+                <Flexbox flexDirection='column'>
+                  <SizedDivisor height='40'/>
+                  <Image src={AnimacaoSuporte}/>
+                </Flexbox> : <div/>
+            }
+            <SizedDivisor height='40'/>
+            <Flexbox style={{width: 'calc(100% - 20px)', margin: '0px 50px'}}>
+              <Input placeholder='Nome' type='string'  onChange={(value) => {
+                    setName(value.target.value)
+                  }}/>
+              <SizedDivisor width='40'/>
+              <Input placeholder='Restaurante' type='string'  onChange={(value) => {
+                    setRestaurantName(value.target.value)
+                  }}/>
             </Flexbox>
-            <SizedDivisor width='10'/>
-            <Flexbox>
-              <RadioButton type="radio" name="contact" value="mail" onChange={ (value) => {
-                  if(value.target.checked) {
-                    setContactForm('mail')
+            <SizedDivisor height='20'/>
+            <Flexbox style={{width: 'calc(100% - 20px)' }}>
+              <Flexbox>
+                <RadioButton type="radio" name="contact" value="phone" onChange={ (value) => {
+                    if(value.target.checked) {
+                      setContactForm('phone')
+                    }
                   }
-                }
-              }/>
-              <SizedDivisor width='8'/>
-              <RadioLabel>Email</RadioLabel>
+                }/>
+                <SizedDivisor width='8'/>
+                <RadioLabel>Telefone</RadioLabel>
+              </Flexbox>
+              <SizedDivisor width='10'/>
+              <Flexbox>
+                <RadioButton type="radio" name="contact" value="mail" onChange={ (value) => {
+                    if(value.target.checked) {
+                      setContactForm('mail')
+                    }
+                  }
+                }/>
+                <SizedDivisor width='8'/>
+                <RadioLabel>Email</RadioLabel>
+              </Flexbox>
+              {
+              isCellphone  ? <div/> :
+              <Flexbox flexDirection='row' style={{flex: '1' }}>
+                <SizedDivisor width='20'/>
+                <Input placeholder='Contato' type='string' style={{ flex: '1' }}   onChange={(value) => {
+                    setContact(value.target.value)
+                  }}/>
+                </Flexbox>
+            }
             </Flexbox>
             {
-            isCellphone  ? <div/> :
-            <Flexbox flexDirection='row' style={{flex: '1' }}>
-              <SizedDivisor width='20'/>
-              <Input placeholder='Contato' type='string' style={{ flex: '1' }}/>
-              </Flexbox>
-          }
-          </Flexbox>
-          {
-            isCellphone  ? 
-            <Flexbox flexDirection='column' style={{width: '100%' }}>
-                <SizedDivisor height='20'/>
-                <Input placeholder='Contato' type='string'/>
-              </Flexbox> : <div/>
-          }
-          <SizedDivisor height='20'/>
-          <BaseTextAreaInput rows={5} placeholder='Relate seu problema neste campo'/>
-          <SizedDivisor height='40'/>
-          <Button>Enviar</Button>
-        </FormWrapper>
-      </Container>
-    </Flexbox>
+              isCellphone  ? 
+              <Flexbox flexDirection='column' style={{width: '100%' }}>
+                  <SizedDivisor height='20'/>
+                  <Input placeholder='Contato' type='string'   onChange={(value) => {
+                    setContact(value.target.value)
+                  }}/>
+                </Flexbox> : <div/>
+            }
+            <SizedDivisor height='20'/>
+            <BaseTextAreaInput rows={5} placeholder='Relate seu problema neste campo'   onChange={(value) => {
+                    setProblemDescription(value.target.value)
+                  }}/>
+            <SizedDivisor height='40'/>
+            <Button onClick={async () => {
+              if(validateForm() && await sendSupportEmail(name, restaurantName, contact, problemDescription)) {
+                setShowSucessPage(true)
+              }
+            }}>Enviar</Button>
+          </FormWrapper>
+        </Container>
+      </Flexbox>
+      }
     </Flexbox>
   </Background>;
 }
